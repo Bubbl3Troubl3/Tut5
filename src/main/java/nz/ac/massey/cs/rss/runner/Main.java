9bfc7514a4447c67ddc01f3ad18d6d396ef019b7
Bubbl3Troubl3 <sxtywei0001@sina.com>
2023-09-24 15:50:23 +1300

    RSS media content

--- src/main/java/nz/ac/massey/cs/rss/runner/Main.java
+++ src/main/java/nz/ac/massey/cs/rss/runner/Main.java
@@ -3,17 +3,14 @@ package nz.ac.massey.cs.rss.runner;
 import nz.ac.massey.cs.sdc.parsers.Rss;
 import nz.ac.massey.cs.sdc.parsers.RssChannel;
 import nz.ac.massey.cs.sdc.parsers.RssItem;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.namespace.QName;
+import javax.xml.bind.*;
 import java.io.File;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.*;
 
 public class Main {
@@ -21,39 +18,52 @@ public class Main {
         JAXBContext jc = JAXBContext.newInstance("nz.ac.massey.cs.sdc.parsers");
         Unmarshaller parser = jc.createUnmarshaller();
         File file = new File("herald.xml");
-        URL url = new URL("http://rss.nzherald.co.nz/rss/xml/nzhrsscid_000000005.xml"); //this url has redirected link
+        URL url = new URL("http://rss.nzherald.co.nz/rss/xml/nzhrsscid_000000005.xml"); //this url has redirected link to an RSS XML source.
 
-        //Rss rss = (Rss) parser.unmarshal(file);       //use this line to read local xml file
-        Rss rss = (Rss) parser.unmarshal(getFinalURL(url));          //this is to read xml from url address; use getFinalURL(url) to retrieve the final url
+        //Rss rss = (Rss) parser.unmarshal(file);                       //uncomment this line to read local xml file example
+        Rss rss = (Rss) parser.unmarshal(getFinalURL(url));             //this is to read xml from url address; use getFinalURL(url) to retrieve the final url
         RssChannel rssChannel = rss.getChannel();
 
         List<RssItem> rssItemList;
         rssItemList = rssChannel.getItem();
 
+        int i = 0;
         for (RssItem rssItem : rssItemList){
+            i++;
+            System.out.println(i);
             List<Object> objList;
-            Map<QName, String> attrMap = new HashMap<>();
             objList = rssItem.getTitleOrDescriptionOrLink();
-            attrMap = rssItem.getOtherAttributes();
-
+            int j = 0;
             for (Object obj : objList){
                 try {
-                    switch (((JAXBElement) obj).getName().toString()) {
-                        case "title":
-                        case "link":
-                        case "description":
-                            System.out.println(((JAXBElement) obj).getName() + ": " + ((JAXBElement) obj).getValue());
-                        default:
+                    if (obj instanceof JAXBElement ) {
+                        switch (((JAXBElement) obj).getName().toString()) {
+                            case "title":
+                            case "link":
+                            case "description":
+                                System.out.println(((JAXBElement) obj).getName() + ": " + ((JAXBElement) obj).getValue());
+                            default:
+                        }
+                    }
+                    /**
+                     * for XML self closing tags with attributes, they are parsed as "ElementNSimpl" object type, but it seems can only be detected as Element type
+                     * e.g. <media:content url="https://www.nzherald.co.nz/resizer/R-Pw53rIHRHyX4ZN4PDljIjXdlc=/460x230/smart/filters:quality(70)/cloudfront-ap-southeast-2.images.arcpublishing.com/nzme/6IRKU5MJV5FXTM4UQ2VIYBWY2A.jpg" type="image/jpeg" height="230" width="460"/>
+                     * These tags are parsed into Element objects, each has a NamedNodeMap data through .getAttributes() method. Then use .getNamedItem("url").getNodeValue to retrieve image/video link info.
+                     */
+                    else if(obj instanceof Element) {
+                        if (((Element) obj).getLocalName().contains("content")) {
+                            System.out.println("Media content" + " " + (j+1) + ":");
+                            //System.out.println(((Element) obj).getTagName());
+                            NamedNodeMap nodeMap = ((Element) obj).getAttributes();
+                            System.out.println(nodeMap.getNamedItem("url").getNodeValue());
+                            j++;
+                        }
                     }
                 }catch(Exception e){
-                    /**url link can parse ElementNSImpl objects which cannot be converted to JAXBElement.
-                     * it appears info I wanted is not in any of these ElementNSImpl objects
-                     * when Exception is thrown due to this, ignore and continue
-                     */
-                    //e.printStackTrace();
+                    e.printStackTrace();
                 }
             }
-            
+
             System.out.println('\n');
         }
     }
@@ -87,5 +97,4 @@ public class Main {
         }
         return url;
     }
-
 }
